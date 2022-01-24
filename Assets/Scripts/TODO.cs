2f2402b12e25b9b26db21ba8a6e021214fde83c0
Chris <chrisaconquet@hotmail.com>
2022-01-24 12:08:36 +0100

    Added UI assets
    - mana
    - End turn
    - enemy intend bg veranderd

---
+++ Assets/Scripts/TODO.cs
@@ -0,0 +1,14 @@
+/*
+
+_______________________________TODO_______________________
+
+    - transition screen needs to play sound
+
+    1. Fix gridmap
+    2. Fix displaycards (you have to go back to gridmap after selecting one) 
+    3. Fix gnomes Y position when spawned
+    4. Implement BloodSplatters on char when hitting maybe bellow 50% hp?
+    5. 
+
+
+*/
